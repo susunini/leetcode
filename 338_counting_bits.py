1c65342746cd86cc173c4c3bd7f3ee80622ee628
susunini <wei.su.2008@gmail.com>
2016-08-10 16:01:39 -0700

    Solution 2

--- 338_counting_bits.py
+++ 338_counting_bits.py
@@ -9,3 +9,16 @@ class Solution(object):
         for i in range(len(r_ints)):
             r_ints[i] = r_ints[i >> 1] + (i & 1)
         return r_ints
+     
+# Solution 2:
+# i&(i-1) drops the lowest one bit of i
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        r_ints = [0] * (1 + num)
+        for i in range(1, len(r_ints)):
+            r_ints[i] = r_ints[i&(i-1)] + 1
+        return r_ints
