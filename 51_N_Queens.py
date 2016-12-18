4d8c3911042930bb3ed4e3471425f3bdbd9a1869
susunini <wei.su.2008@gmail.com>
2016-12-18 13:01:09 -0800

    Create 51_N_Queens.py

---
+++ 51_N_Queens.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    """
+    a solution <=> a valid list of column indexes to position queens
+    (x1, y1) and (x2, y2) are in the same diagonal line <=> abs(x1 - x2) == abs(y1 - y2) OR
+    [(x1 + y1) == (x2 + y2) or (x1 - y1) == (x2 - y2)]
+    """
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        def dfs(row, cols, xy_sums, xy_diffs):
+            if len(cols) == n:
+                sols.append(cols)
+                return
+            for col in range(n):
+                if col not in cols and (row + col) not in xy_sums and (row - col) not in xy_diffs:
+                    dfs(row+1, cols+[col], xy_sums+[row+col], xy_diffs+[row-col])
+            
+        sols = []
+        dfs(0, [], [], [])
+        return [['.'*col + 'Q' + '.'*(n-col-1) for col in sol] for sol in sols]
