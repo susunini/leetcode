f8810083531b3d6423dcaff320e13cab1eade0df
susunini <wei.su.2008@gmail.com>
2017-02-08 12:42:56 -0800

    Create 23_Merge_k_Sorted_Lists.py

---
+++ 23_Merge_k_Sorted_Lists.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+import heapq
+
+class Solution(object):
+    """ Linked List. Heaqp. 
+    
+    We can also divide and conquer to use merge two lists at a time. 
+    """
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        cur = dummy
+        h = [(head.val, head) for head in lists if head]
+        heapq.heapify(h)
+        while h:
+            _, node = heapq.heappop(h)
+            cur.next = node; cur = cur.next
+            if node.next:
+                heapq.heappush(h, (node.next.val, node.next))
+            
+        return dummy.next
+        
+        
+  
