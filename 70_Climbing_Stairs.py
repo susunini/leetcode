916aff2ac83ddc528a1969e11d591171e2397f5a
susunini <wei.su.2008@gmail.com>
2017-02-15 16:58:10 -0800

    Create 70_Climbing_Stairs.py

---
+++ 70_Climbing_Stairs.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    """ Dynamic Programming.
+    
+    dp[i] - number of ways to step i
+    dp[1] = 1; dp[2] = 2
+    dp[i] = dp[i-1] + dp[i-2]
+    return dp[n] 
+    """
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 2:
+            return 1
+        dp = [1, 2]
+        for i in range(3, n+1):
+            dp[0], dp[1] = dp[1], dp[0] + dp[1]
+        return dp[-1]
