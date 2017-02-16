60d287c2c96b21f0b2d40f2d819e0c1b3c7ad58f
susunini <wei.su.2008@gmail.com>
2017-02-15 16:39:22 -0800

    Create 96_Unique_Binary_Search_Trees.py

---
+++ 96_Unique_Binary_Search_Trees.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    """ Dynamic Programming. 
+    
+    dp[n]
+    dp[i] = sum(max(dp[j], 1), max(dp[i-j-1], 1) for j from 0 to i-1)"""
+    dp[0] = 0
+    """
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0] * (n + 1)
+        for i in range(1, n + 1):
+            dp[i] = sum([max(dp[j], 1)*max(dp[i-j-1], 1) for j in range(0, i)])
+        return dp[-1]
