0a281f7b3dda99bd5398d9740f04e72eb1b72559
susunini <wei.su.2008@gmail.com>
2017-02-17 12:34:59 -0800

    Update 140_Word_Break_II.py

--- 140_Word_Break_II.py
+++ 140_Word_Break_II.py
@@ -17,8 +17,37 @@ class Solution(object):
                         dp[i].append(word_li + [w])
         res = [' '.join(word_li) for word_li in dp[len(s)-1]]
         return res
+    
+class Solution(object):
+    """ Wrong. """
+    def isBreakable(self, s, wordDict):
+        dp = [False]*(len(s)+1)
+        for i in range(len(s)):
+            for w in wordDict:
+                if (i-len(w)+1) >= 0 and s[i-len(w)+1:i+1] == w:
+                    if dp[i-len(w)+1] is True:
+                        dp[i+1] = True
+                        break
+        return dp[-1]
         
- class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: List[str]
+        """
+        dp = collections.defaultdict(list)
+        dp[-1] = [[]]
+        for i in range(len(s)):
+            for w in wordDict:
+                if (i-len(w)+1) >= 0 and s[i-len(w)+1:i+1] == w:
+                    for word_li in dp[i-len(w)]:
+                        dp[i].append(word_li + w)
+        res = [' '.join(word_li) for word_li in dp[len(s)-1]]
+        return res
+
+        
+class Solution(object):
     """ Dynamic Programming.
     
     Add a checker to avoid TLE for large data. """
