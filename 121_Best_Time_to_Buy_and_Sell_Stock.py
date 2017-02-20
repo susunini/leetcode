060776698dbf331c4fc263d24d4c71b2de0e4ea2
susunini <wei.su.2008@gmail.com>
2017-02-20 10:20:17 -0800

    Update 121_Best_Time_to_Buy_and_Sell_Stock.py

--- 121_Best_Time_to_Buy_and_Sell_Stock.py
+++ 121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -32,3 +32,25 @@ class Solution(object):
             res = max(res, price-min_price)
             min_price = min(min_price, price)
         return res
+    
+class Solution(object):
+    """ A variation of the orginal problem.
+    
+    if the interviewer twists the question slightly by giving the difference array of prices, Ex: for {1, 7, 4, 11}, 
+    if he gives {0, 6, -3, 7}, the problem is same as 53 Maxium Subarray. """
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+        diffs = [0] * len(prices)
+        for i in range(1, len(diffs)):
+            diffs[i] = prices[i]-prices[i-1]
+        dp = 0
+        res = 0
+        for diff in diffs[1:]:
+            dp = max(0, dp) + diff
+            res = max(res, dp)
+        return res
