ef8d2153b2063fc14f19f8aecf860aa24217d214
susunini <wei.su.2008@gmail.com>
2017-03-03 15:06:48 -0800

    Update 146_LRU_Cache.py

--- 146_LRU_Cache.py
+++ 146_LRU_Cache.py
@@ -2,7 +2,7 @@ class LRUCache(object):
     """ Design. Data Structure.
     
     Use a dictionary and a python list.
-    Python list (array internally) is expensive with del and insert.
+    Python list (array internally) is expensive with delete and insert.
     """
 
     def __init__(self, capacity):
@@ -13,7 +13,6 @@ class LRUCache(object):
         self.d = dict()
         self.li = list()
         
-
     def get(self, key):
         """
         :type key: int
@@ -27,8 +26,6 @@ class LRUCache(object):
         self.li.insert(0, key)
         return self.d[key]
         
-        
-
     def put(self, key, value):
         """
         :type key: int
@@ -61,9 +58,78 @@ class Node(object):
         self.prev = None
         
 class LRUCache(object):
+    """ Wrong. """
+    def addNode(self, node):
+        """ Add a node after head of the list """
+        tmp = self.head.next
+        self.head.next = node
+        node.prev = self.head
+        node.next = tmp
+        tmp.prev = node
+    
+    def removeNode(self, node):
+        """ Remove a node from the list """
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        node.prev = node.next = None
+    
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.cap = capacity
+        self.cnt = 0
+        self.key_to_node = {}
+        self.head = Node(None, None)
+        self.tail = Node(None, None)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        
+    def get(self, key):
+        """
+        :rtype: int
+        """
+        if key in self.key_to_node:
+            node = self.key_to_node[key]
+            self.removeNode(node)
+            self.addNode(node)
+            return node.value
+        else:
+            return -1
+
+    def set(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: nothing
+        """
+        if key in self.key_to_node:
+            node = self.key_to_node[key]
+            node.value = value
+            self.removeNode(node)
+            self.addNode(node)
+        else:
+            new_node = Node(key, value)
+            self.key_to_node[key] = new_node
+            self.addNode(new_node)
+            self.cnt += 1
+            # caution: do not forget
+            if self.cnt > self.cap:
+                node_to_del = self.tail.prev
+                del self.key_to_node[node_to_del.value]
+                self.removeNode(node_to_del)
+                self.cnt -= 1
+        
+class LRUCache(object):
     """ Use a dictionary and a double linked list to keep record of
     LRU; Add two functions addNode and removeNode and all the operations
     with the double linked list can be done using these two functions.
+    
+    The dictionary stores key to node instead of key to value to save the
+    effort of locating a value in the double linked list.
+    
+    Use a double linked list instead of a standard linked list mainly for
+    locating the LRU node, which is the node previous to tail.
     """
     def addNode(self, node):
         """ Add a node after head of the list """
