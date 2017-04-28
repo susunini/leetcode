ac43d52f4da3c51264691e63535a4df13b74643c
susunini <wei.su.2008@gmail.com>
2017-04-27 17:28:11 -0700

    Update 140_Word_Break_II.py

--- 140_Word_Break_II.py
+++ 140_Word_Break_II.py
@@ -50,7 +50,9 @@ class Solution(object):
 class Solution(object):
     """ Dynamic Programming.
     
-    Add a checker to avoid TLE for large data. """
+    Add a checker to avoid TLE for large data. 
+    Use defaultdict of list. 
+    """
     def isBreakable(self, s, wordDict):
         dp = [False]*(len(s)+1)
         dp[0] = True
@@ -79,3 +81,30 @@ class Solution(object):
                         dp[i].append(word_li + [w])
         res = [' '.join(word_li) for word_li in dp[len(s)-1]]
         return res
+
+class Solution(object):
+    """ Dynamic Programming.
+    
+    Use a list of list.
+    """
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: List[str]
+        """
+        if not self.isWordBreakable(s, wordDict):
+            return []
+        n = len(s)
+        dp = [[] for _ in range(n)]
+        for i in range(len(s)):
+            for w in wordDict:
+                if (i-len(w)+1) >= 0 and s[i-len(w)+1:i+1] == w:
+                    prev_dp = dp[i-len(w)] if (i-len(w)) >= 0 else [[]]
+                    for w_li in prev_dp:
+                        dp[i].append(w_li + [w])
+        res = []
+        res = [' '.join(word_li) for word_li in dp[len(s)-1]]
+        return res
+            
+        
