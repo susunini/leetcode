2519b9dcaf87b5591f714ae21e51a39c28e4b40c
susunini <wei.su.2008@gmail.com>
2017-05-02 14:20:47 -0700

    Update 338_Counting_Bits.py

--- 338_Counting_Bits.py
+++ 338_Counting_Bits.py
@@ -1,3 +1,15 @@
+class Solution(object):
+    """ Wrong"""
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        res = [0] * (num+1)
+        for i in range(1, num+1):
+            res[i] = res[i>>1] + (i&1)
+        return res
+    
 # Solution 1: My solution beats 26%
 class Solution(object):
     def countBits(self, num):
