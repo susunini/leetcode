b3185bb76100f87eae6632724e88eb38271ac78e
susunini <wei.su.2008@gmail.com>
2017-05-22 13:32:49 -0700

    Update 297_Serialize_and_Deserialize_Binary_Tree.py

--- 297_Serialize_and_Deserialize_Binary_Tree.py
+++ 297_Serialize_and_Deserialize_Binary_Tree.py
@@ -6,7 +6,10 @@
 #         self.right = None
 
 class Codec:
-   """ Tree. Pre-order Traversal with 'null'. 356 ms. """
+    """ Tree. Pre-order Traversal with 'null'. 356 ms. 
+   
+    Follow pre-order to serialize and deserialize. 
+    """
 
     def serialize(self, root):
         """Encodes a tree to a single string.
@@ -44,3 +47,41 @@ class Codec:
             root.right = buildTree()
             return root
         return buildTree()
+
+class Codec:
+    """ 362 ms. """
+    def preorder(self, root):
+        if not root:
+            self.res += '#' if not self.res else ' #'
+            return
+        self.res += str(root.val) if not self.res else ' ' + str(root.val)
+        self.preorder(root.left)
+        self.preorder(root.right)
+        
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        self.res = ''
+        self.preorder(root)
+        return self.res
+        
+    def buildTree(self, data):
+        val = data.pop(0)
+        if val == '#':
+            return None
+        root = TreeNode(int(val))
+        root.left = self.buildTree(data)
+        root.right = self.buildTree(data)
+        return root
+        
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        data = data.split(' ')
+        return self.buildTree(data)
