4c313bfe220db190dd3635d20e6756fc2ff6508f
susunini <wei.su.2008@gmail.com>
2017-05-30 14:54:00 -0700

    Create 222_Count Complete_Tree_Nodes.py

---
+++ 222_Count Complete_Tree_Nodes.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    """ Tree. Recursive. 
+    
+    Number of nodes in a full binary tree = 2^k - 1 = 1 << k - 1 where k is tree height
+    """
+    def countNodes(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        cur = root.left
+        left_height = 0
+        while cur:
+            left_height += 1
+            cur = cur.left
+        cur = root.right
+        right_height = 0
+        while cur:
+            right_height += 1
+            cur = cur.left
+        print left_height, right_height
+        if left_height == right_height:
+            return (1 << left_height) + self.countNodes(root.right)
+        else:
+            return (1 << right_height) + self.countNodes(root.left)
