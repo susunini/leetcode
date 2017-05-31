80c6ad666540f05272d7fdab1d1ae2be3456a64e
susunini <wei.su.2008@gmail.com>
2017-05-31 11:14:50 -0700

    Update 572_Subtree_of_Another_Tree.py

--- 572_Subtree_of_Another_Tree.py
+++ 572_Subtree_of_Another_Tree.py
@@ -6,7 +6,69 @@
 #         self.right = None
 
 class Solution(object):
-    """ Tree. Recursive. """
+    """ Wrong. """
+    def dfs(self, root, node, res):
+        if not root:
+            return
+        if root.val == node.val:
+            res.append(root)
+        self.dfs(root.left, node, res)
+        self.dfs(root.right, node, res)
+        
+    def isSameTree(self, root1, root2):
+        if (not root1 or not root2) and (root1 != root2):
+            return False
+        if root1.val != root2.val:
+            return False
+        return self.isSameTree(root1.left, root2.left) and self.isSameTree(root1.right, root2.right)
+        
+    def isSubtree(self, s, t):
+        """
+        :type s: TreeNode
+        :type t: TreeNode
+        :rtype: bool
+        """
+        nodes = []
+        self.dfs(s, t, nodes)
+        for node in nodes:
+            if self.isSameTree(node, t):
+                return True
+        return False
+    
+class Solution(object):
+    """ Tree. 522ms(26%). """
+    def dfs(self, root, node, res):
+        if not root:
+            return
+        if root.val == node.val:
+            res.append(root)
+        self.dfs(root.left, node, res)
+        self.dfs(root.right, node, res)
+        
+    def isSameTree(self, root1, root2):
+        if root1 == root2:
+            return True
+        if not root1 or not root2:
+            return False
+        if root1.val != root2.val:
+            return False
+        return self.isSameTree(root1.left, root2.left) and self.isSameTree(root1.right, root2.right)
+        
+    def isSubtree(self, s, t):
+        """
+        :type s: TreeNode
+        :type t: TreeNode
+        :rtype: bool
+        """
+        nodes = []
+        self.dfs(s, t, nodes)
+        for node in nodes:
+            if self.isSameTree(node, t):
+                return True
+        return False
+    
+class Solution(object):
+    """ Tree. Recursive. 402ms(60%). """
     def isSame(self, s, t):
         if not s and not t:
             return True
