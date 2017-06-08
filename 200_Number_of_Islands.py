f5f6195556db9c2b55fe370d5ef3bf8d9a8774f3
susunini <wei.su.2008@gmail.com>
2017-06-08 12:59:06 -0700

    Create 200_Number_of_Islands.py

--- 200_Number_of_Islands.py
+++ 200_Number_of_Islands.py
@@ -25,6 +25,22 @@ class Solution(object):
                     self.dfs(i, j, grid)
         return res
     
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        def dfs(i, j):
+            if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == '1':
+                grid[i][j] = 'X'
+                map(dfs, (i+1, i-1, i, i), (j, j, j+1, j-1))
+                return 1
+            return 0
+        return sum(
+            dfs(i,j) for i in range(len(grid)) for j in range(len(grid[i]))
+            )
+    
 """ Union Find. Wrong. """
 class IslandUnionFind(object):
     def __init__(self, grid):
