397ca768d17f236d0789edda79267bd7f3c01dd9
susunini <wei.su.2008@gmail.com>
2017-06-12 13:07:24 -0700

    Create 307_Range_Sum_Query_Mutable.py

--- 307_Range_Sum_Query_Mutable.py
+++ 307_Range_Sum_Query_Mutable.py
@@ -24,7 +24,7 @@ class NumArray(object):
         :param end: end index
         :return root node of tree
         """
-        if start > end: # Wrong: did not have this stop condition error with corner case: nums=[]
+        if start > end: # did not have this stop condition causing error of corner case: nums=[]
             return None
         node = SegmentTreeNode(start, end)
         if start == end:
@@ -109,7 +109,112 @@ class NumArray(object):
         """
         return self.sumRangeTree(i, j, self.root)
         
+class NumArray(object):
+    """ Segment Tree. Using a list to values(integers) to represent a segment tree.
+    Record only values of each node. Get start and end from function parameters.
+    Binary Tree:
+    1. maxiumum height of a complete binary tree of n leaf nodes (here complete binary tree is defined as each non-leaf nodes have exactly two
+    children) is log2(n)
+    2. Maxium number of nodes in a binary tree of height h is (1 << h + 1).
+    3. In a list representing a complete binary tree defined as above, if index of current node is i, then index of left node = 2*i+1 and
+    index of right node = 2*i+2.
+    
+    """
+    
+    def wrong__init__(self, nums):
+        """
+        wrong when nums==[]
+        :type nums: List[int]
+        """
+        self.nums = nums
+        h = int(math.ceil(math.log(len(nums), 2)))
+        self.nodes = [0]*(1<<h+1)
+        self.buildTree(0, len(nums)-1, 0)
+    
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = nums
+        if not nums:
+            return
+        h = int(math.ceil(math.log(len(nums), 2)))
+        self.nodes = [0]*(1<<h+1)
+        self.buildTree(0, len(nums)-1, 0)
+        
+    def wrong_buildTree(self, start, end, cur):
+        """wrong *only* when nums == []"""
+        if start == end:
+            self.nodes[cur] = self.nums[start]
+        else:  
+            mid = start+(end-start)/2
+            left, right = 2*cur+1, 2*cur+2
+            self.nodes[cur] = self.buildTree(start, mid, left) + self.buildTree(mid+1, end, right)
+        return self.nodes[cur]
+        
+    def buildTree(self, start, end, cur):
+        if start > end:
+            return 0
+        if start == end:
+            self.nodes[cur] = self.nums[start]
+        else:  
+            mid = start+(end-start)/2
+            left, right = 2*cur+1, 2*cur+2
+            self.nodes[cur] = self.buildTree(start, mid, left) + self.buildTree(mid+1, end, right)
+        return self.nodes[cur]
+        
+    def updateTree(self, i, val, start, end, cur):
+        if start == end == i:
+            self.nodes[cur] = val
+            return
+        mid = start+(end-start)/2
+        left, right = 2*cur+1, 2*cur+2
+        if i <= mid:
+            self.updateTree(i, val, start, mid, left)
+        else:
+            self.updateTree(i, val, mid+1, end, right)
+        self.nodes[cur] = self.nodes[left] + self.nodes[right]
+        
+    def sumRangeTree(self, i, j, start, end, cur):
+        if start == i and end == j:
+            return self.nodes[cur]
+        mid = start+(end-start)/2
+        left = 2*cur+1; right = 2*cur+2
+        if j <= mid:
+            return self.sumRangeTree(i, j, start, mid, left)
+        elif i > mid:
+            return self.sumRangeTree(i, j, mid+1, end, right)
+        else:
+            return self.sumRangeTree(i, mid, start, mid, left) + self.sumRangeTree(mid+1, j, mid+1, end, right)
+            
 
+    def update(self, i, val):
+        """
+        :type i: int
+        :type val: int
+        :rtype: void
+        """
+        self.nums[i] = val
+        return self.updateTree(i, val, 0, len(self.nums)-1, 0)
+        
+        
+
+    def sumRange(self, i, j):
+        """
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        if i == j:
+            return self.nums[i]
+        return self.sumRangeTree(i, j, 0, len(self.nums)-1, 0)
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# obj.update(i,val)
+# param_2 = obj.sumRange(i,j)
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
