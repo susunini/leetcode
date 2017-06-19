f19f76c629a84f75ae04a109f7baeddff1022c5d
susunini <wei.su.2008@gmail.com>
2017-06-19 11:31:35 -0700

    Update 269_Alien_Dictionary.py

--- 269_Alien_Dictionary.py
+++ 269_Alien_Dictionary.py
@@ -30,6 +30,48 @@ class Solution(object):
                     indeg[end].remove(start)
             leaves = [char for char, char_set in indeg.items() if not char_set]
         return ''.join(res) if not indeg else ''
+    
+class Solution(object):
+    """ Graph. Topological Sort. 49ms(49%). """
+    def getFirstDiff(self, word1, word2):
+        n = min(len(word1), len(word2))
+        for i in range(n):
+            if word1[i] != word2[i]:
+                return i
+        return None
+        
+    def alienOrder(self, words):
+        """
+        :type words: List[str]
+        :rtype: str
+        """
+        unique = set(''.join(words))
+        n = len(unique)
+        
+        indeg = collections.defaultdict(int)
+        outdeg = collections.defaultdict(list)
+        for i in range(1, len(words)):
+            j = self.getFirstDiff(words[i-1], words[i])
+            if j != None:  # Wrong- if j: 
+                start, end = words[i-1][j], words[i][j]
+                indeg[end] += 1
+                outdeg[start].append(end)
+                
+        res = []
+        leaves = [ch for ch in unique if indeg[ch] == 0]
+        while leaves:
+            res += leaves
+            n -= len(leaves)
+            new_leaves = []
+            for start in leaves:
+                for end in outdeg[start]:
+                    indeg[end] -= 1
+                    if indeg[end] == 0:
+                        new_leaves.append(end)
+            leaves = new_leaves
+                        
+        return ''.join(res) if n == 0 else ''
+            
             
                 
         
