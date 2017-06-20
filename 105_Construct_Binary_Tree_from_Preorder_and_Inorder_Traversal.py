85874c1e2eafe6239ab4d4d9f7ac17eb36564b96
susunini <wei.su.2008@gmail.com>
2017-06-20 12:00:40 -0700

    Create 105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py

---
+++ 105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    """ Tree. 50%.
+    Preorder DFS. """
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if not preorder:
+            return None
+        root = TreeNode(preorder[0])
+        i = inorder.index(root.val)
+        root.left = self.buildTree(preorder[1:i+1], inorder[:i])
+        root.right = self.buildTree(preorder[i+1:], inorder[i+1:])
+        return root
