1b181ee2979263cdd84b44b8f98868ee7162587c
susunini <wei.su.2008@gmail.com>
2017-06-27 14:37:23 -0700

    Update 347_Top_K_Frequent_Elements.py

--- 347_Top_K_Frequent_Elements.py
+++ 347_Top_K_Frequent_Elements.py
@@ -26,3 +26,27 @@ class Solution(object):
             dic[num] += 1
         h = [[val, key] for key, val in dic.items()]
         return [key for val, key in heapq.nlargest(k, h)]
+    
+class Solution(object):
+    """ Bucket Sort. """
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        
+        dic = collections.defaultdict(int)
+        for num in nums:
+            dic[num] += 1
+        
+        max_freq = max([value for _, value in dic.items()])   
+        buckets = [[] for i in range(max_freq + 1)]
+        for num, cnt in dic.items():
+            buckets[cnt].append(num)
+        res = []
+        for i in range(len(buckets) - 1, -1, -1):
+            if len(res) == k:
+                return res
+            if buckets[i]:
+                res.extend(buckets[i])
