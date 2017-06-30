fdfe6a6ff74cbf7c17308733bfbf1abd6eb968a9
susunini <wei.su.2008@gmail.com>
2017-06-30 11:46:49 -0700

    Create 35_Search_Insert_Position.py

---
+++ 35_Search_Insert_Position.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    """ Binary Search. """
+    def helper(self, nums, target, start, end):
+        mid = start+(end-start)/2
+        if target == nums[mid]:
+            return mid
+        if (end-start) == 1:
+            return end
+        if target < nums[mid]:
+            return self.helper(nums, target, start, mid)
+        else:
+            return self.helper(nums, target, mid, end)
+        
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        if target < nums[0]:
+            return 0
+        if target > nums[-1]:
+            return len(nums)
+        return self.helper(nums, target, 0, len(nums)-1)
