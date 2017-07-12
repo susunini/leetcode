ef870eb5727b4c5f100eadc43f324eea790dbc32
susunini <wei.su.2008@gmail.com>
2017-07-12 16:09:51 -0700

    Update 273_Integer_to_English_Words.py

--- 273_Integer_to_English_Words.py
+++ 273_Integer_to_English_Words.py
@@ -2,6 +2,7 @@ LESS_THEN_20 = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eigh
 TENS = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
 
 class Solution(object):
+    """ 55%. """
     def numberToWords(self, num):
         """
         :type num: int
@@ -24,3 +25,34 @@ class Solution(object):
             return ' '.join([self.helper(num/1000000), 'Million', self.helper(num%1000000)]).strip()
         else:
             return ' '.join([self.helper(num/1000000000), 'Billion', self.helper(num%1000000000)]).strip()
+        
+        
+LESS_THEN_20 = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+TENS = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+THOUSANDS = ['', 'Thousand', 'Million', 'Billion']
+
+class Solution(object):
+    """ 96%. """
+    def numberToWords(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if num == 0:
+            return 'Zero'
+        return self.helper(num)
+    
+    def helper(self, num):
+        if num < 20:
+            return LESS_THEN_20[num]
+        if num < 100:
+            return ' '.join([TENS[num/10], self.helper(num%10)]).strip()
+        if num < 1000:
+            return ' '.join([self.helper(num/100), 'Hundred', self.helper(num%100)]).strip()
+        i = 1
+        while num >= pow(1000, i+1):
+            i += 1
+        print i
+        return ' '.join([self.helper(num/pow(1000, i)), 
+                         THOUSANDS[i], 
+                         self.helper(num%pow(1000, i))]).strip()
