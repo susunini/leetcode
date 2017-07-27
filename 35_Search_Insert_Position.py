d7c897a2b353e4961570041c1e5dc5ac315b434e
susunini <wei.su.2008@gmail.com>
2017-07-27 14:59:43 -0700

    Update 35_Search_Insert_Position.py

--- 35_Search_Insert_Position.py
+++ 35_Search_Insert_Position.py
@@ -24,3 +24,28 @@ class Solution(object):
         if target > nums[-1]:
             return len(nums)
         return self.helper(nums, target, 0, len(nums)-1)
+    
+ class Solution(object):
+    """ 84%. """
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start = 0; end = len(nums)-1
+        while start+1 < end:
+            mid = start+(end-start)/2
+            if target == nums[mid]:
+                return mid
+            if target < nums[mid]:
+                end = mid
+            else:
+                start = mid
+        if target in [nums[start], nums[end]]:
+            return start if target == nums[start] else end
+        if target < nums[start]:
+            return start
+        if target > nums[end]:
+            return end+1
+        return end
