afcb6aed6b1bb43d060a65db7c557ecebc40d7fe
susunini <wei.su.2008@gmail.com>
2017-08-12 16:46:21 -0700

    Create 131_Palindrome_Partitioning.py

---
+++ 131_Palindrome_Partitioning.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        p1 = 0; p2 = len(s)-1
+        while p1 < p2:
+            if s[p1] != s[p2]:
+                return False
+            p1 += 1; p2 -= 1
+        return True
+    
+    def helper(self, s, solution, solutions):
+        if not s:
+            solutions.append(solution)
+        for i in range(len(s)):
+            if self.isPalindrome(s[:i+1]):
+                self.helper(s[i+1:], solution+[s[:i+1]], solutions)
+                
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        solutions = list()
+        self.helper(s, [], solutions)
+        return solutions
