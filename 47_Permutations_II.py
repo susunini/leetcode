6c0bd0235b6bfbe4e625d255e3ae2ccf5366ea6d
susunini <wei.su.2008@gmail.com>
2017-08-11 19:32:49 -0700

    Create 47_Permutations_II.py

---
+++ 47_Permutations_II.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def helper(self, nums, permutation, result):
+        if not nums:
+            result.append(permutation[:])
+        for i, num in enumerate(nums):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            self.helper(nums[:i]+nums[i+1:], permutation+[num], result)
+            
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = list()
+        self.helper(nums, [], result)
+        return result
