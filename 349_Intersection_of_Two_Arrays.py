70317001797ff2dd2a74e0914e4cb631a6978677
susunini <wei.su.2008@gmail.com>
2017-08-15 15:53:55 -0700

    Create 349_Intersection_of_Two_Arrays.py

---
+++ 349_Intersection_of_Two_Arrays.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    """ Solution 1: Hash Table. """
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1_dict = {num:True for num in nums1}
+        return list(set([num for num in nums2 if num in nums1_dict]))
+        
+class Solution(object):
+    """ Sort. """
+    def intersection(self, nums1, nums2):
+        nums1.sort(); nums2.sort()
+        result = set()
+        p1 = p2 = 0
+        while p1 < len(nums1) and p2 < len(nums2):
+            num1 = nums1[p1]; num2 = nums2[p2]
+            if num1 == num2:
+                result.add(num1)
+                p1 += 1; p2 += 1  # Miss
+            elif num1 > num2:
+                p2 += 1
+            else:
+                p1 += 1
+        return list(result)
+        
+class Solution(object):
+    """ Sort and Binary Search. """
+    def intersection(self, nums1, nums2):
+        pass
