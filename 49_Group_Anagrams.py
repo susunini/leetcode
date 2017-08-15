083ed53f924fd02f119302c14e11d8ba2d5d714f
susunini <wei.su.2008@gmail.com>
2017-08-14 18:33:30 -0700

    Update 49_Group_Anagrams.py

--- 49_Group_Anagrams.py
+++ 49_Group_Anagrams.py
@@ -1,8 +1,7 @@
 class Solution(object):
     def getHashKey(self, cur_str):
-        return ''.join(sorted(cur_str))
+        return ''.join(sorted(cur_str))  # Wrong key of dict cannot be list: return sorted(cur_cur)
             
-        
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
@@ -11,6 +10,6 @@ class Solution(object):
         match = collections.defaultdict(list)
         for cur_str in strs:
             match[self.getHashKey(cur_str)].append(cur_str)
-        return [li for _, li in match.items()]
+        return [li for _, li in match.items() if len(li) > 1]
             
         
