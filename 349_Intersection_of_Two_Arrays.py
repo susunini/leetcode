49fa1d84a9fc857e853ac55cdc4671b036c923d3
susunini <wei.su.2008@gmail.com>
2017-08-19 16:17:36 -0700

    Update 349_Intersection_of_Two_Arrays.py

--- 349_Intersection_of_Two_Arrays.py
+++ 349_Intersection_of_Two_Arrays.py
@@ -25,6 +25,26 @@ class Solution(object):
             else:
                 p1 += 1
         return list(result)
+    
+class Solution:
+    """ Sort wo set. """
+    def intersection(self, nums1, nums2):
+        nums1.sort(); nums2.sort()
+        p1 = p2 = 0
+        result = list()
+        while p1 < len(nums1) and p2 < len(nums2):
+            while (p1+1) < len(nums1) and nums1[p1] == nums1[p1+1]:
+                p1 += 1
+            while (p2+1) < len(nums2) and nums2[p2] == nums2[p2+1]:
+                p2 += 1
+            if nums1[p1] == nums2[p2]:
+                result.append(nums1[p1])
+                p1 += 1; p2 += 1
+            elif nums1[p1] < nums2[p2]:
+                p1 += 1
+            else:
+                p2 += 1
+        return result
         
 class Solution(object):
     """ Sort and Binary Search. """
