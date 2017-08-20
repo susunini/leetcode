c08ca50930b094a32da74d6a2bd2dcc5085b6c2c
susunini <wei.su.2008@gmail.com>
2017-08-19 17:03:30 -0700

    Update LintCode_Maximum_Subarray_II.py

--- LintCode_Maximum_Subarray_II.py
+++ LintCode_Maximum_Subarray_II.py
@@ -6,21 +6,27 @@ class Solution:
         """ 
         @param nums: A list of integers
         @return: An integer denotes the sum of max two non-overlapping subarrays
+        
+        dp1[i] - maximum subarray for nums[:i]
+        dp2[i] - maximum subarray for nums[i:]
         """
+        if len(nums) < 2:
+            return None
         n = len(nums)
-        maxLeftToRight = [0]*n
-        maxRightToLeft = [0]*n
-        maxLeftToRight[0] = nums[0]
+        dp1 = [0]*n; dp2 = [0]*n
+        prev = nums[0]
+        dp1[0] = nums[0]
         for i in range(1, n):
-            maxLeftToRight[i] = max(0, maxLeftToRight[i-1]) + nums[i]
-        for i in range(1, n):
-            maxLeftToRight[i] = max(maxLeftToRight[i-1], maxLeftToRight[i])
-        maxRightToLeft[-1] = nums[-1]
+            prev = max(prev, 0) + nums[i]
+            dp1[i] = max(dp1[i-1], prev)
+        prev = nums[-1]
+        dp2[-1] = nums[-1]
         for i in range(n-2, -1, -1):
-            maxRightToLeft[i] = max(0, maxRightToLeft[i+1]) + nums[i]
-        for i in range(n-2, -1, -1):
-            maxRightToLeft[i] = max(maxRightToLeft[i+1], maxRightToLeft[i])
-        result = 0
+            prev = max(prev, 0) + nums[i]
+            dp2[i] = max(dp2[i+1], prev)
+        result = -sys.maxint-1
         for i in range(n-1):
-            result = max(result, maxLeftToRight[i]+maxRightToLeft[i+1])
+            result = max(result, dp1[i]+dp2[i+1])
         return result
+    
+    
