844de92c527d1bf75061d082f1d8f8e65c3a48d8
susunini <wei.su.2008@gmail.com>
2017-08-20 10:01:22 -0700

    Update Lintcode_Subarray_Sum.py

--- Lintcode_Subarray_Sum.py
+++ Lintcode_Subarray_Sum.py
@@ -1,6 +1,8 @@
 class Solution:
     """Array. Prefix Sum. 
-    sum[i,j] = sum[0,j] - sum[0,i].
+    Prefix Sum. sum[i,j] = sum[0,j] - sum[0,i].
+    The problem only expect one result.
+    The problem assumes that there is always an answer.
     """
     def subarraySum(self, nums):
         """
@@ -9,9 +11,9 @@ class Solution:
         and the index of the last number
         """
         sum_to_index = {0: -1} # Wrong for test case [-1,1], [0, ...] : sum_to_index = {}
-        sum = 0
+        cur_sum = 0
         for i, num in enumerate(nums):
-            sum += num
-            if sum in sum_to_index:
-                return [sum_to_index[sum]+1, i]
-            sum_to_index[sum] = i
+            cur_sum += num
+            if cur_sum in sum_to_index:
+                return [sum_to_index[cur_sum]+1, i] # Wrong: return [sum_to_index[cur_sum], i]
+            sum_to_index[cur_sum] = i
