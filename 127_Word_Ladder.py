aa8e380bb52b2ee13e6b16cd9b0e430543e485d6
susunini <wei.su.2008@gmail.com>
2017-08-22 13:25:36 -0700

    Update 127_Word_Ladder.py

--- 127_Word_Ladder.py
+++ 127_Word_Ladder.py
@@ -32,3 +32,43 @@ class Solution(object):
                 front, back = back, front
             wordDict -= front
         return 0
+    
+ class Solution(object):
+    """ 20170822. Made some mistakes worth noticing. """
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
+        if endWord not in wordList: # Miss
+            return 0
+        wordList = set(wordList)
+        wordList.discard(beginWord)
+        # wordList.add(endWord) # Don't do that
+        
+        start = set([beginWord]); end = set([endWord])
+        length = 1
+        while start:
+            if start & end:
+                return length
+            next_start = set()
+            for word in start:
+                wordList.discard(word)
+                for i in range(len(word)):
+                    for ch in 'abcdefghijklmnopqrstuvwxyz':
+                        next_word = word[:i]+ch+word[i+1:]
+                        if next_word in wordList:
+                            next_start.add(next_word)
+                            # wordList.remove(next_word) # Don't do that; use wordList -= start after 
+                                                         # swapping start and end
+            start = next_start
+            length += 1
+            if len(start) > len(end):
+                start, end = end, start 
+            wordList -= start
+        return 0
+            
+            
+        
