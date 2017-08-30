2d1a7ba2ada588b123a6f625fc47c512c783046f
susunini <wei.su.2008@gmail.com>
2017-08-29 22:49:23 -0700

    Update 70_Climbing_Stairs.py

--- 70_Climbing_Stairs.py
+++ 70_Climbing_Stairs.py
@@ -14,6 +14,6 @@ class Solution(object):
         if n < 2:
             return 1
         dp = [1, 2]
-        for i in range(3, n+1):
+        for i in range(3, n+1): # Wrong: for i in range(3, n)
             dp[0], dp[1] = dp[1], dp[0] + dp[1]
         return dp[-1]
