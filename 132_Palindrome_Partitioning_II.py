266d2c00f43356e1b71b62a34ebeeda2e792b140
susunini <wei.su.2008@gmail.com>
2017-08-31 10:54:32 -0700

    Update 132_Palindrome_Partitioning_II.py

--- 132_Palindrome_Partitioning_II.py
+++ 132_Palindrome_Partitioning_II.py
@@ -29,6 +29,37 @@ class Solution:
                 else:
                     dp[i][j] = dp[i+1][j-1] and (s[i] == s[j])
         return dp
+    
+class Solution(object):
+    def minCut(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        matrix = self.getPalindromeMatrix(s)
+        n = len(s)
+        dp = [i for i in range(n)]
+        for i in range(n):
+            if matrix[0][i]:
+                dp[i] = 0
+                continue
+            for j in range(0, i):
+                if matrix[j+1][i]:
+                    dp[i] = min(dp[i], dp[j]+1)
+        return dp[n-1]
+
+    def getPalindromeMatrix(self, s):
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        for i in range(n-1, -1, -1): # Wrong: for i in range(n)
+            for j in range(i, n):
+                if i == j:
+                    dp[i][j] = True
+                elif j - i == 1:
+                    dp[i][j] = (s[i] == s[j])
+                else:
+                    dp[i][j] = (s[i] == s[j]) and dp[i+1][j-1]
+        return dp
                     
         
         
