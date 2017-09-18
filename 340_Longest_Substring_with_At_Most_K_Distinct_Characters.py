a718454de1261d0d0c84fa1514fb310790a3041c
susunini <wei.su.2008@gmail.com>
2017-09-18 13:21:34 -0700

    Update 340_Longest_Substring_with_At_Most_K_Distinct_Characters.py

--- 340_Longest_Substring_with_At_Most_K_Distinct_Characters.py
+++ 340_Longest_Substring_with_At_Most_K_Distinct_Characters.py
@@ -20,3 +20,26 @@ class Solution(object):
                 p1 = min_index+1
         res = max(res, p2-p1)
         return res
+    
+class Solution(object):
+    """ 20170918. 99%. 模板. """
+    def lengthOfLongestSubstringKDistinct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        result = 0
+        ch_index = dict()
+        start = 0
+        for end, ch in enumerate(s):
+            ch_index[ch] = end
+            if len(ch_index) <= k:
+                continue
+            result = max(result, end-start)
+            while len(ch_index) > k:
+                if ch_index[s[start]] == start:
+                    del ch_index[s[start]]
+                start += 1
+        result = max(result, len(s)-start)
+        return result
