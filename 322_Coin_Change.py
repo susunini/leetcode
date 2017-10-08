bacba8e2f0069438e3f5aaf0b72d6f1a4a5206c2
susunini <wei.su.2008@gmail.com>
2017-10-07 19:40:00 -0700

    Update 322_Coin_Change.py

--- 322_Coin_Change.py
+++ 322_Coin_Change.py
@@ -12,3 +12,13 @@ class Solution(object):
             li = [dp[i-c] for c in coins if i-c >= 0 and dp[i-c] >= 0]
             dp[i] = min(li) + 1 if li else -1
         return dp[amount]
+    
+ class Solution(object):
+    def coinChange(self, coins, amount):
+        MAX = float('inf')
+        dp = [0] + [MAX] * amount
+
+        for i in xrange(1, amount + 1):
+            dp[i] = min([dp[i - c] if i - c >= 0 else MAX for c in coins if i-c >= 0]) + 1
+
+        return [dp[amount], -1][dp[amount] == MAX]
