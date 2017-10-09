146d116a7e6f488c550b52ae0a75e527c1e11cd4
susunini <wei.su.2008@gmail.com>
2017-10-08 21:16:58 -0700

    Create Twitter_word_dictionary_k_edit_distance.py

---
+++ Twitter_word_dictionary_k_edit_distance.py
@@ -0,0 +1,64 @@
+"""
+Given a dictionary and a word w, find all words from the dictionary whose edit distance from w is k.
+"""
+
+class TrieNode:
+    def __init__(self):
+        self.word = None
+        self.children = {}
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for i, letter in enumerate(word):
+            if letter not in node.children: 
+                node.children[letter] = TrieNode()
+            node = node.children[letter]
+        node.word = word
+
+def buildTrie(wordDict):
+    trie = Trie()
+    for word in wordDict:
+        trie.insert(word)
+    return trie
+
+def search(trie, word, max_dist):
+    results = list()
+    cur_row = range(len(word)+1) # cur_row = [i for i in range(len(word)+1)]
+    print cur_row
+    for letter, child in trie.root.children.items():
+        doSearch(child, letter, word, max_dist, cur_row, results)
+    return results
+
+
+def doSearch(node, letter, word, max_dist, prev_dp_row, results):
+    cur_dp_row = [None]*(len(word)+1)
+    cur_dp_row[0] = prev_dp_row[0]+1
+    for j in range(1, len(cur_dp_row)):
+        insert_cost = prev_dp_row[j]+1
+        delete_cost = cur_dp_row[j-1]+1
+        replace_cost = prev_dp_row[j-1] + (0 if letter == word[j-1] else 1)
+        cur_dp_row[j] = min(insert_cost, delete_cost, replace_cost)
+    print cur_dp_row
+    if cur_dp_row[-1] <= max_dist and node.word is not None:
+        results.append(node.word)
+
+    if min(cur_dp_row) > max_dist:
+        return
+
+    for letter, child in node.children.items():
+        doSearch(child, letter, word, max_dist, cur_dp_row, results)
+
+dictionary = ['bat', 'batt', 'cat', 'beetle']
+trie = buildTrie(dictionary)
+assert(search(trie, 'bat', 1) == ['cat', 'bat', 'batt'])
+assert(search(trie, 'beatle', 1) == ['beetle'])
+
+
+
+
+
