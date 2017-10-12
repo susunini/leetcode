e5e470a67071612bbd1db6270855f23626f8a9cd
susunini <wei.su.2008@gmail.com>
2017-10-12 12:33:41 -0700

    Update 239_Sliding_Window_Maximum.py

--- 239_Sliding_Window_Maximum.py
+++ 239_Sliding_Window_Maximum.py
@@ -17,3 +17,21 @@ class Solution(object):
                 heapq.heappop(heap)
             result.append(-heap[0][0])
         return result
+    
+class Solution(object):
+    """ Deque. O(n). 71%.
+    deque is a double linked list supporting O(1) pop and push from both ends. 
+    deque 1. sotres indexes 2. values in deque are decreasing 3. max is always at head. 
+    """
+    def maxSlidingWindow(self, nums, k):
+        d = collections.deque()
+        out = []
+        for i, num in enumerate(nums):
+            while d and nums[d[-1]] < num:
+                d.pop()
+            d += i,
+            if d[0] == i - k:
+                d.popleft()
+            if i >= k - 1:
+                out += nums[d[0]],
+        return out
